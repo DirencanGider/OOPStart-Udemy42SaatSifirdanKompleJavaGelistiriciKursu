51e6a23698c65d6414f941da350f02e53448ed14
DirencanGider <direncangider@hotmail.com>
2023-11-28 16:00:03 +0300

    Commit Message - 28.11.2023 16:00

--- Car.java
+++ Car.java
@@ -1,10 +1,22 @@
 public class Car {
 
-    public String colour;
-    public int doors;
-    public int wheels;
-    public String motor;
-    public String model;
+    private String colour;
+    private int doors;
+    private int wheels;
+    private String motor;
+    private String model;
+
+    //These are all features that we added this Car class.
+
+
+    public void setModel (String model) { //Set methods are used to reach private objects with different ways in another classes.
+
+        this.model = model; /*We used "this" keyword because both of words were same. We had to match the model that feature and model that we will assign in Test class.
+                            If we assigned another different word like "model1", it wouldnt necessary. Directly we could equal them.*/
+    }
+    public String getModel() { //Get methods are used to call private objects with different ways in another classes.
+        return this.model;
+    }
 
 
 }
--- Test.java
+++ Test.java
@@ -1,18 +1,22 @@
-import java.sql.SQLOutput;
 
 public class Test {
     public static void main(String[] args) {
 
-        Car car1 = new Car(); //We created a new object from Car class with car1 reference.
+        Car car1 = new Car(); //We created a new object from Car class with car1 reference. Yes, car1 is a reference.
 
+        /*
         car1.colour = "Devil's Red";
         car1.doors = 4;
         car1.wheels = 4;
         car1.motor = "16V";
         car1.model = "ZR1";
-
         System.out.println(car1.motor);
         System.out.println(car1.colour);
+         */
+
+
+
+
 
     }
 
