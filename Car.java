85394ee025d783219b33af7ba8608902e2765848
DirencanGider <direncangider@hotmail.com>
2023-12-07 12:22:24 +0300

    Commit Message - 07.12.2023 12:23

--- Car.java
+++ Car.java
@@ -18,5 +18,45 @@ public class Car {
         return this.model;
     }
 
+    /* We are able to set setter and getter method those all in this page shortly.
+    Right click in this area > Refractor > Encapsulate Fields > Choose Setter and Getter block that you want to refractor > Click Refractor below
+    In Intellij Idea Community Edition, Encapsulation Fields is disabled. We need to follow this instructions;
+    Right click > Generate > Getter and Setter > Select Features of Object > Generate */
 
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        if (wheels < 0) {
+            System.out.println("Wheels number cannot be under 0");
+        }
+        else {
+            this.wheels = wheels;
+        }
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
 }
--- Test.java
+++ Test.java
@@ -14,12 +14,21 @@ public class Test {
         System.out.println(car1.colour);
          */
 
-        car1.setModel("ZR1");
+        /*car1.setModel("ZR1");
 
-        System.out.println("Car model: " + car1.getModel());
+        System.out.println("Car model: " + car1.getModel());*/
 
 
+        car1.setWheels(-4);
 
+        System.out.println("Wheels number : " + car1.getWheels());
+
+
+        Car car2 = null; /*If we don't identify object and equal "null" value, Java doesn't show any error in command window.
+                         But if we run in this way, IDE gives an error like "Exception in thread "main" java.lang.NullPointerException:
+                         Cannot invoke "Car.setModel(String)" because "car2" is null at Test.main(Test.java:28)"*/
+
+        car2.setModel("Renault");
 
 
     }
